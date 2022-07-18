1905ebcac7e70b0d046486e9e175db6e92ffb36e
HouR-BeaN <92734226+HouR-BeaN@users.noreply.github.com>
2022-07-18 19:59:58 +0800

    Update inidex.js

--- backend/inidex.js
+++ backend/inidex.js
@@ -6,7 +6,7 @@ const MongoClient = mongodb.MongoClient;
 
 const port = process.env.PORT || 8000;
 
-MongoClient.connect(processs.env.RESTREVIEWS_DB_URI, {
+MongoClient.connect(process.env.RESTREVIEWS_DB_URI, {
   poolSize: 50,
   wtimeout: 2500,
   useNewUrlParse: true,
